711872d6c3afbba868494d0428166007db66c919
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-07-18 14:08:22 +0200

    Add project files.

---
+++ api101/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace api101.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ api101/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="api101.WebApiApplication" Language="C#" %>
